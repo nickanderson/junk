933c9cc4ebc56a6c01d0d05a2d07de9d822780aa
Nick Anderson <nick@cmdln.org>
2022-06-06 13:28:07 -0500

    Update install-cfengine-hub-version-master.sh

--- install-cfengine-hub-version-master.sh
+++ install-cfengine-hub-version-master.sh
@@ -1,4 +1,4 @@
 URL=$(cf-remote --version master list hub ubuntu20 | tail -n 1)
 curl -O $URL
 PACKAGE_FILENAME=$(basename $URL)
-apt install ./$PACKAGE_FILENAME
+sudo apt install ./$PACKAGE_FILENAME
