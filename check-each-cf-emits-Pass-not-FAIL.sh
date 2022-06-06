450d915e87c06881fe9d74752e6d348604f3314e
Nick Anderson <nick@cmdln.org>
2022-06-06 14:03:49 -0500

    Update check-each-cf-emits-Pass-not-FAIL.sh

--- check-each-cf-emits-Pass-not-FAIL.sh
+++ check-each-cf-emits-Pass-not-FAIL.sh
@@ -1,8 +1,20 @@
+FAILURES=0
 for each in $(ls | grep "\.cf$"); do
+  echo "Running $each"
   output=$(cf-agent -Kf $each)
   if [ "echo $output | grep Pass" ] && [ "echo $output | grep -v FAILED" ]; then
-    exit 0
+    echo "Passed $each"
   else
-    exit 1
+    FAILURES=$((FAILURES+1))
+    echo "FAILED $each"
+    echo "cf-agent -Kf $each:"
+    echo "$output"
   fi
 done
+if [ $FAILURES -eq 0 ]; then
+  # Non zero failures, fail
+  exit 1
+else
+  # No Failures
+  exit 0
+fi
