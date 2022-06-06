b6859152ded2905f803dd2f7bb5959afd44e3670
Nick Anderson <nick@cmdln.org>
2022-06-06 13:31:21 -0500

    Update install-cfengine-hub-version-master.sh

--- install-cfengine-hub-version-master.sh
+++ install-cfengine-hub-version-master.sh
@@ -2,3 +2,8 @@ URL=$(cf-remote --version master list hub ubuntu20 | tail -n 1)
 curl -O $URL
 PACKAGE_FILENAME=$(basename $URL)
 sudo apt install ./$PACKAGE_FILENAME
+
+# Setup un-privledged execution
+mkdir -p ~/.cfagent/inputs
+ln -s /var/cfengine/share/NovaBase/masterfiles/lib ~/.cfagent/inputs/lib
+find ~/.cfagent
